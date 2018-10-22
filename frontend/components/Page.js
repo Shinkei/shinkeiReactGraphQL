4c7246626b5ee54fcad74e16075cfc1afab45049
Shinkei <ing.jorgeivanramirezh@gmail.com>
2018-10-21 21:43:45 -0500

    Style the App and the Header also create a theme for styled Components
    Create a theme for style components, also give some style to the Page
    component and the header components, due to the header component is
    a dump component I change it for a function component

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,25 +1,42 @@
 import React from 'react';
+import styled, { ThemeProvider, injectGlobal } from 'styled-components';
 import Header from './Header';
 import Meta from './Meta';
-import styled from 'styled-components';
 
-const PageButton = styled.button`
-  background: red;
+const theme = {
+  red: '#FF0000',
+  black: '#393939',
+  grey: '#3A3A3A',
+  lightgrey: '#E1E1E1',
+  offWhite: '#EDEDED',
+  maxWidth: '1000px',
+  bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)',
+};
+
+const StyledPage = styled.div`
+  background: white;
+  color: ${props => props.theme.black};
   font-size: ${props => props.huge};
-  color: ${props => props.blanco ? 'white' : 'black'};
+`;
+
+const InnerStyle = styled.div`
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+  padding: 2rem;
 `;
 
 class Page extends React.Component {
   render() {
     return (
-      <>
-        <Meta />
-        <Header />
-        <p>Page Component</p>
-        <PageButton huge="8px" >Click me</PageButton>
-        <PageButton blanco >Click me</PageButton>
-        {this.props.children}
-      </>
+      <ThemeProvider theme={theme}>
+        <StyledPage>
+          <Meta />
+          <Header />
+          <InnerStyle>
+            {this.props.children}
+          </InnerStyle>
+        </StyledPage>
+      </ThemeProvider>
     );
   }
 }
