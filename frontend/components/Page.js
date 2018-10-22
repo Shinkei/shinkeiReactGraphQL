143a7356c19d3fca05256ff56240bc2e30b4f71b
Shinkei <ing.jorgeivanramirezh@gmail.com>
2018-10-21 20:45:06 -0500

    Add a TEST of styled components
    create a styled component button and send some values through the props
    like color an size, this is just a test to see the features of style
    components

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import Header from './Header';
 import Meta from './Meta';
+import styled from 'styled-components';
+
+const PageButton = styled.button`
+  background: red;
+  font-size: ${props => props.huge};
+  color: ${props => props.blanco ? 'white' : 'black'};
+`;
 
 class Page extends React.Component {
   render() {
@@ -9,6 +16,8 @@ class Page extends React.Component {
         <Meta />
         <Header />
         <p>Page Component</p>
+        <PageButton huge="8px" >Click me</PageButton>
+        <PageButton blanco >Click me</PageButton>
         {this.props.children}
       </>
     );
